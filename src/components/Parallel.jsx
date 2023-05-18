680603dde32faa0fa851e22073ba05c1203bc223
Juan David Peña <juandavidp76@hotmail.com>
2023-05-18 12:41:30 -0500

    fix: comments

--- src/components/Parallel.jsx
+++ src/components/Parallel.jsx
@@ -1,33 +1,44 @@
 import { useEffect, useState } from 'react';
 
+
+//hago la peticion de los episodios
 async function getEpisodes() {
   const response = await fetch('https://rickandmortyapi.com/api/episode/');
   const data = await response.json();
   return data.results;
 }
 
+//hago la peticion de la url de los capitulos de los personajes
 async function getCharacters(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
+
+//solicito la data
 async function getData() {
   const episodes = await getEpisodes();
 
+  //filtro los episodios para que cada uno me muestre los 10 primeros personajes
   const charEpisodes = episodes.reduce((acum, item) => {
     return [...acum, ...item.characters.slice(0, 10)];
   }, []);
 
+//obtengo las url de los capitulos donde aparece cada personaje
   const chPromise = charEpisodes.map((url) => {
     return getCharacters(url);
   });
+
+  //hago la solicitud en paralelo
   const result = await Promise.all(chPromise);
 
+  //obtendo la data y la recorro
   const data = episodes.map((episode) => {
     return {
       title: `${episode.name} - ${episode.episode}`,
       dateToAir: episode.air_date,
+      //filtro de la url de los personajes y la comparo con la de los episodios
       characters: episode.characters.slice(0, 10).map((url) => {
         return result.find((item) => item.url === url);
       }),
@@ -37,14 +48,17 @@ async function getData() {
 }
 
 function Parallel() {
+  //variable de estado
   const [data, setData] = useState([]);
 
+  //lo uso para solicitar solo una vez los datos
   useEffect(() => {
     getData().then((data) => {
       setData(data);
     });
   }, []);
 
+  //se pinta en el explorador la info solicitada
   return (
     <div>
       <h1>Rick and Morty Parallel Fetch</h1>
